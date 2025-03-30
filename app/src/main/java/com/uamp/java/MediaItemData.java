07870ef46721023b23ac874b5921b297051a3da9
이용인 <yongin0517@gmail.com>
2025-03-31 03:39:12 +0900

    Fix: MediaItemData 정적 변수 수정

--- app/src/main/java/com/uamp/java/MediaItemData.java
+++ app/src/main/java/com/uamp/java/MediaItemData.java
@@ -12,6 +12,28 @@ import java.util.Objects;
 
 
 public class MediaItemData {
+    public static final Integer PLAYBACK_RES_CHANGED = 1;
+    public static final DiffUtil.ItemCallback<MediaItemData> diffCallback = new DiffUtil.ItemCallback<MediaItemData>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull @NotNull MediaItemData mediaItemData, @NonNull @NotNull MediaItemData t1) {
+            return Objects.equals(mediaItemData.mediaId, t1.mediaId);
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull @NotNull MediaItemData mediaItemData, @NonNull @NotNull MediaItemData t1) {
+            return Objects.equals(mediaItemData.mediaId, t1.mediaId) && Objects.equals(mediaItemData.playbackRes, t1.playbackRes);
+        }
+
+        @Nullable
+        @Override
+        public @org.jetbrains.annotations.Nullable Object getChangePayload(@NonNull @NotNull MediaItemData oldItem, @NonNull @NotNull MediaItemData newItem) {
+            if (!Objects.equals(oldItem.playbackRes, newItem.playbackRes)) {
+                return PLAYBACK_RES_CHANGED;
+            } else {
+                return null;
+            }
+        }
+    };
     public final String mediaId;
     public final String title;
     public final String subtitle;
@@ -26,50 +48,5 @@ public class MediaItemData {
         this.albumArtUri = albumArtUri;
         this.browable = browable;
         this.playbackRes = playbackRes;
-    }
-
-    public static MediaItemData getInstance() {
-        final Integer PLAYBACK_RES_CHANGED = 1;
-        /**
-         * [DiffUtil.ItemCallback] for a [MediaItemData].
-         *
-         * Since all [MediaItemData]s have a unique ID, it's easiest to check if two
-         * items are the same by simply comparing that ID.
-         *
-         * To check if the contents are the same, we use the same ID, but it may be the
-         * case that it's only the play state itself which has changed (from playing to
-         * paused, or perhaps a different item is the active item now). In this case
-         * we check both the ID and the playback resource.
-         *
-         * To calculate the payload, we use the simplest method possible:
-         * - Since the title, subtitle, and albumArtUri are constant (with respect to mediaId),
-         *   there's no reason to check if they've changed. If the mediaId is the same, none of
-         *   those properties have changed.
-         * - If the playback resource (playbackRes) has changed to reflect the change in playback
-         *   state, that's all that needs to be updated. We return [PLAYBACK_RES_CHANGED] as
-         *   the payload in this case.
-         * - If something else changed, then refresh the full item for simplicity.
-         */
-        final DiffUtil.ItemCallback<MediaItemData> diffCallback = new DiffUtil.ItemCallback<MediaItemData>() {
-            @Override
-            public boolean areItemsTheSame(@NonNull @NotNull MediaItemData mediaItemData, @NonNull @NotNull MediaItemData t1) {
-                return Objects.equals(mediaItemData.mediaId, t1.mediaId);
-            }
-
-            @Override
-            public boolean areContentsTheSame(@NonNull @NotNull MediaItemData mediaItemData, @NonNull @NotNull MediaItemData t1) {
-                return Objects.equals(mediaItemData.mediaId, t1.mediaId) && Objects.equals(mediaItemData.playbackRes, t1.playbackRes);
-            }
-
-            @Nullable
-            @Override
-            public @org.jetbrains.annotations.Nullable Object getChangePayload(@NonNull @NotNull MediaItemData oldItem, @NonNull @NotNull MediaItemData newItem) {
-                if (!Objects.equals(oldItem.playbackRes, newItem.playbackRes)) {
-                    return PLAYBACK_RES_CHANGED;
-                } else {
-                    return null;
-                }
-            }
-        };
     }
 }
